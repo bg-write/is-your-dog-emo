afc7071c9c50bf062d545db6611755974e140038
bg-write <bradywgerber@gmail.com>
2020-11-16 14:59:08 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,79 @@
+console.log('santiy check');
+/*---------------- Pseudo-Code (Move to README) -------------*/
+// Add the HTML button that'll activate both APIs; when I press this button, I want both APIs to pull data and return their results to the app.
+// Add container element for the 'box' components to be appended to as children; I need two container elements, one for each API data result.
+// Add cached element references for the container elements; create the JS code that will select these two boxes and allows us to play with them and make them do things. (.getElementById, or querySelector/All.)
+// Add a cached element reference for the button; create the JS code that'll select the button and allow us to play with it and make it do things. (.getElementById, or querySelector/All.)
+// Add an event listener to the button; I want to tell JS that when the user clicks on the button, something will happen. (.addEventListener('click', ...))
+// Write code for API call and examine response data (test with console.log); When both APIs are called, I want them to return a random value from the API (NOTE: This will likely take the most time to figure out how to apply select.random logic to the data I'm pulling.)
+// Tweak event listeners so that the APIs are stored in a variable (test with console.log); Actually put those API calls into those boxes I defined earlier
+// Create a test 'box' element with Bootstrap; start making this look nice and actually return the data I want and how I want it to look
+// Add a function to handle appending a 'box' containing the API to the container element; I want to be able to restart the app with the button and return a new random value for each API.
+// Put this all together through a fancy render function! (NOTE: This also will be my biggest challenge going into this, beside the random data logic.)
+// Style each box differently, based on what API is being pulled; get creative
+// Once logic is sound and turns data I want, make app more visually appealing and creative; incorporate images, animations, sounds, etc
+// End my CSS design with at least one responsive media query; if needed
+// Add confetti; for Ben :)
+
+
+
+/*------------------- Project Outline -----------------------*/
+                /* ROUND1: Gather The Bones */
+
+
+
+                /* ROUND2: Create The Skeleton */
+
+
+
+            /* ROUND3: Make The Skeleton Come Alive */
+
+
+
+            /* ROUND4: Make The Skeleton Spooky */
+
+
+
+/*----------- Transparency Report (Move to README) ----------*/
+
+
+
+/*-------- Questions Upon Completion (Move to README) -------*/
+
+
+
+/*---------------- Constants --------------------------------*/
+/* OBJECTS THAT DON'T CHANGE AND WE'LL WANT TO REFER TO AND USE OFTEN */
+
+
+
+/*---------------- Variables --------------------------------*/
+/* OBJECTS THAT WE CAN REDIRECT AND MANIPULATE TO HELP US DO THINGS */
+
+
+
+/*--------- Cached Element References -----------------------*/
+/* Allows us to select and manipulate the DOM elements  */
+/* document.getElementById('<element>') */
+/* document.querySelector('<element>') */
+/* document.querySelectorAll('<element>') */
+
+
+
+/*------------- Event Listeners -----------------------------*/
+/* Allows us to register an event listener for a given event; if we 'click' on something, something now happens */
+/* <SELECT-DOM>.addEventListerner('<event-name>', <callback>); */
+
+
+
+/*----------------- Functions -------------------------------*/
+/* reuseable blocks of code performing one task; my verbs */
+
+
+
+/*----------------- And Scene -------------------------------*/
+/* and so it is
+
+-bg
+
+(project finished to initial completion and satisfaction: XXX) */
