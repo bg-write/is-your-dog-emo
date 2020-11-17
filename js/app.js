9ba023cdf74121e2fdc896334db255b08ad4b32c
bg-write <bradywgerber@gmail.com>
2020-11-17 14:04:42 -0500

    JS: first draft: functions

--- js/app.js
+++ js/app.js
@@ -50,37 +50,34 @@ console.log('hi. welcome to "is your dog emo."');
 
 
 /*---------------- Constants --------------------------------*/
-/* OBJECTS THAT DON'T CHANGE AND WE'LL WANT TO REFER TO AND USE OFTEN */
 // Make my objects!
 
 
 
 /*---------------- Variables --------------------------------*/
-/* OBJECTS THAT WE CAN REDIRECT AND MANIPULATE TO HELP US DO THINGS */
-// Make my variables!
-
+const dogs = [];
 
 
 /*--------- Cached Element References -----------------------*/
-//// Add cached element references for the container elements; create the JS code that will select these two boxes and allows us to play with them and make them do things. (.getElementById, or querySelector/All.)
-//// Add a cached element reference for the button; create the JS code that'll select the button and allow us to play with it and make it do things. (.getElementById, or querySelector/All.)
 // I will need to make a cached element reference for my messageText, but right now I want to make sure I can return my APIs, which are my priority.
 const dogContainer = document.getElementById('randomDogAPIImage');
+
 const genreContainer = document.getElementById('genrenatorAPIText');
 const activateBtn = document.getElementById('activateButton');
 
 
 
 /*------------- Event Listeners -----------------------------*/
-/* <SELECT-DOM>.addEventListerner('<event-name>', <callback>); */
-// Add an event listener to the button; I want to tell JS that when the user clicks on the button, something will happen. (.addEventListener('click', ...))
 activateBtn.addEventListener('click', ()=> {
     fetch("https://random.dog/woof.json")
     .then((response) => {
         return response.json();
     })
-    .then((data) => {
-        console.log(data);
+    .then((dog) => {
+        let newDog = {};
+        newDog["dog"] = dog.dog;
+        dogs.push(newDog);
+        render();
     })
     .catch((err) => {
         console.log(err);
@@ -90,11 +87,18 @@ activateBtn.addEventListener('click', ()=> {
 
 
 /*----------------- Functions -------------------------------*/
-/* reuseable blocks of code performing one task; my verbs */
-// Write code for API call and examine response data (test with console.log); When both APIs are called, I want them to return a random value from the API (NOTE: This will likely take the most time to figure out how to apply select.random logic to the data I'm pulling.)
-// Tweak event listeners so that the APIs are stored in a variable (test with console.log); Actually put those API calls into those boxes I defined earlier
-// Add a function to handle appending a 'box' containing the API to the container element; I want to be able to restart the app with the button and return a new random value for each API.
-// Put this all together through a fancy render function! (NOTE: This also will be my biggest challenge going into this, beside the random data logic.)
+function appendDiv(dog, idx) {
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = `${dog}`;
+    container.appendChild(newDiv);
+} // Add a function to handle appending a 'card' containing the quote to the container element.
+
+function render() {
+    container.innerHTML = "";
+    dogs.forEach((dog, idx) => {
+      appendDiv(dog["dog"], idx);
+    });
+} 
 
 
 
