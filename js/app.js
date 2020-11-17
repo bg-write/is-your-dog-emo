c833eda210c42b4fcb124957b631c536c88786d2
bg-write <bradywgerber@gmail.com>
2020-11-17 11:33:16 -0500

    add initial classes, IDs

--- js/app.js
+++ js/app.js
@@ -1,29 +1,35 @@
 console.log('hi. welcome to "is your dog emo."');
 /*------------------ Project Outline -----------------------*/
 /* ROUND1: Gather The Bones */
-// h1 // id="headline" class="flex-container"
+// h1 // id="headlineText" class="flex-container"
 // static string section that just reads: "is your dog emo." Every other element on the app main page will change, so I want at least one element to stay consistent and keep the eyes engaged throughout the app usage for the user. The overall app "theme" will stem from the presentation of this h1.
 
 // h2 // id="messageText" class="flex-container"
-//
+// dynamic string section that returns a string message based on the results of the genrenatorAPI string. If the genrenatorAPI returns a string that contains "emo", then I want to print out a special message saying: "your dog is emo." Otherwise, return "no." If I have time, I want to hide this message, but put out confetti anytime an emo genre comes up randomly. This is the container that will have the most flexiblity and be the most dependent on the results of my button and two divs.
 
-// div // id="genrenatorAPI" class="flex-container"
-//
+// div // id="randomDogAPIImage" class="flex-container"
+// dynamic image section that returns whatever random image is called by the randomDog API. When I click the activateButton, a new image will pop up here. I might run into some tricky parts of making sure the data from the API transfers over correctly to show up on the app screen.
 
-// div // id="randomDogAPI" class="flex-container"
-//
+// div // id="genrenatorAPIText" class="flex-container"
+// dynamic string section that returns whatever random string is called by the Genrenator API. When I click the activateButton, a new string will pop up here. The results of this string will determine the results of my MessageText.
 
-// button // id="activateButton" class="flex-container"
-//
+// div // id="buttonArea" class="flex-container"
+// This is the holder for my button, which is required to make flex-container work with the button.
+
+// button // id="activateButton"
+// button that initiates a click function that calls both APIs. When I click this button, I want to call both the Genrerator and randomDog APIs in their own separate container elements, which will then determine the result of my messageText results. If I have time, I want the button to first read "let's find out." when a user first uses the app; when they get a result and wish to try again, I want the button to change its text to "try again." When a user refreshes the entire page, the text should return to "let's find out."
 
 
 
 /* ROUND2: Create The Skeleton */
 // FLEXBOX
-// | column | column | column | 
-// | column | column | column | 
-// | column | column | column | 
-// | column | column | column | 
+// | id="headlineText" |
+// | id-"messageText" |
+// | id="randomDogAPIImage"  |
+// | id="genrenatorAPIText" |
+// | id="buttonArea" |
+// 1 column, 5 rows
+
 
 
 /* ROUND3: Make The Skeleton Come Alive */
