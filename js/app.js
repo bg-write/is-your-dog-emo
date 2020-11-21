40d677387db3627f7e89826eb90d2bc31b617b65
bg-write <bradywgerber@gmail.com>
2020-11-21 09:42:00 -0500

    Update DRYclean: JS

--- js/app.js
+++ js/app.js
@@ -1,111 +1,57 @@
 console.log('hi. welcome to "is your dog emo."');
-/*------------------ Project Outline (for planning) -----------------------*/
-/* ROUND1: Gather The Bones */
-
-// h1 // id="headlineText" class="flex-container"
-// static string section that just reads: "is your dog emo." Every other element on the app main page will change, so I want at least one element to stay consistent and keep the eyes engaged throughout the app usage for the user. The overall app "theme" will stem from the presentation of this h1.
-
-// h2 // id="messageText" class="flex-container"
-// dynamic string section that returns a string message based on the results of the genrenatorAPI string. If the genrenatorAPI returns a string that contains "emo", then I want to print out a special message saying: "your dog is emo." Otherwise, return "no." If I have time, I want to hide this message, but put out confetti anytime an emo genre comes up randomly. This is the container that will have the most flexiblity and be the most dependent on the results of my button and two divs.
-
-// div // id="randomDogAPIImage" class="flex-container"
-// dynamic image section that returns whatever random image is called by the randomDog API. When I click the activateButton, a new image will pop up here. I might run into some tricky parts of making sure the data from the API transfers over correctly to show up on the app screen.
-
-// div // id="genrenatorAPIText" class="flex-container"
-// dynamic string section that returns whatever random string is called by the Genrenator API. When I click the activateButton, a new string will pop up here. The results of this string will determine the results of my MessageText.
-
-// div // id="buttonArea" class="flex-container"
-// This is the holder for my button, which is required to make flex-container work with the button.
-
-// button // id="activateButton"
-// button that initiates a click function that calls both APIs. When I click this button, I want to call both the Genrerator and randomDog APIs in their own separate container elements, which will then determine the result of my messageText results. If I have time, I want the button to first read "let's find out." when a user first uses the app; when they get a result and wish to try again, I want the button to change its text to "try again." When a user refreshes the entire page, the text should return to "let's find out."
-
-
-
-/* ROUND2: Create The Skeleton */
-// FLEXBOX
-// | h1 / id="headlineText" |
-// | h2 / id-"messageText" |
-// | div / id="randomDogAPIImage"  |
-// | div / id="genrenatorAPIText" |
-// | div id="buttonArea" |
-// 1 column, 5 rows
-
-
-
-/* ROUND3: Make The Skeleton Come Alive */
-// See below
-
-
-/* ROUND4: Make The Skeleton Spooky */
-// see below
-
-
-
-/*--------------- Constants --------------------------------*/
-
-
-
-/*--------------- Variables --------------------------------*/
-
-
 
 /*-------- Cached Element References -----------------------*/
+// actives the button to make it ready for an event listener
 const activateBtn = document.getElementById('activateButton');
 
 
 
 /*------------ Event Listeners -----------------------------*/
-activateBtn.addEventListener('click', ()=> {
-    fetch("https://random.dog/woof.json")
-    .then((response) => {
-        return response.json();
+// event listener connecting the button to the randomDog API; when you click the button, a random dog photo shows up on the browser
+activateBtn.addEventListener('click', ()=> { // when you click on the button, the following event happens ...
+    fetch("https://random.dog/woof.json") // fetch the randomDog API ...
+    .then((response) => { // and then return it as a promise (a response) ...
+        return response.json(); // ... and then return the extracted JSON content from the response.
     })
-    .then((dog) => {
-            console.log(Object.values(dog)[1]);
-            let newDog = document.getElementById('randomDogAPIImage');
-            newDog.innerHTML = `<div id="dogImageHolder" class="flex-container"><img src="${Object.values(dog)[1]}" onerror="src='https://thumbs.dreamstime.com/z/funny-dog-wearing-wig-female-clothes-white-background-48441429.jpg'" alt="dog." width="350px" height="300px"></div>`;
+    .then((dog) => { // then we do the following to the promise, now defined as "dog" ...
+            // console.log(Object.values(dog)[1]); // turn this on for testing
+            let newDog = document.getElementById('randomDogAPIImage'); // create "newDog" and have it set to ID randomDogAPIImage ...
+            newDog.innerHTML = `<div id="dogImageHolder" class="flex-container"><img src="${Object.values(dog)[1]}" onerror="src='https://thumbs.dreamstime.com/z/funny-dog-wearing-wig-female-clothes-white-background-48441429.jpg'" alt="dog." width="350px" height="300px"></div>`; // In "newDog", we now update it with a new div, which we ID as "dogImageHolder" and link it to the value (url) of the API. Also added a default stock photo image to replace any broken links, set the alt text to "dog." and gave it a specific widthe and height.
     })
-    .catch((err) => {
-        console.log(err);
+    .catch((err) => { // finally, we catch any errors ...
+        console.log(err); // and upload them to our console.
     });
-}); // This console.logs a new dog photo with each click of the button AND gets it to the screen! I need to edit the CSS to adjust all photos to certain sizes. And what to do to edit out the error images and the videos?
+});
 
 
-// Genrenator API call to the same button!
-activateBtn.addEventListener('click', ()=> {
-    fetch("https://binaryjazz.us/wp-json/genrenator/v1/genre/")
-    .then((response) => {
-        return response.json();
+
+// event listener connecting the button to the Genrenator API; when you click the button, a random genre string shows up on the browser
+activateBtn.addEventListener('click', ()=> { // when you click on the button, the following event happens ...
+    fetch("https://binaryjazz.us/wp-json/genrenator/v1/genre/") // fetch the Genrenator API ...
+    .then((response) => { // and then return it as a promise (a response) ...
+        return response.json(); // ... and then return the extracted JSON content from the response.
     })
-    .then((genre) => {
-        console.log(genre);
-        let newGenre = document.getElementById('genrenatorAPIText');
-        newGenre.innerHTML = `${genre}.`;
-        // the above works fine, don't change it for now!!!
-        let newGenreString = JSON.stringify(genre); // turns genre name into a string, don't change
-        let newGenreStringUpperCase = newGenreString.toUpperCase(); // uppercase the string, don't change!
-        if (newGenreStringUpperCase.includes('EMO')) {
-            console.log('yes.');
-            let newMessageEmo = document.getElementById('messageText');
-            newMessageEmo.innerHTML = `(yes.)`;
-            confetti.start(1500); // confetti!
-        } else {
-            console.log('no.');
-            let newMessageEmo = document.getElementById('messageText');
-            newMessageEmo.innerHTML = `(no.)`;
+    .then((genre) => { // then we do the following to the promise, now defined as "genre" ..
+        // console.log(genre); // turn this on for testing
+        let newGenre = document.getElementById('genrenatorAPIText'); // create "newGenre" and have it set to ID genrenatorAPIText ...
+        newGenre.innerHTML = `${genre}.`; // populate "newGenre" with the API ...
+        let newGenreString = JSON.stringify(genre); // turn genre into string ...
+        let newGenreStringUpperCase = newGenreString.toUpperCase(); // uppercase genre ...
+        if (newGenreStringUpperCase.includes('EMO')) { // and establish new if statement; if the genre includes "EMO" ...
+            // console.log('yes.'); // turn this on for testing
+            let newMessageEmo = document.getElementById('messageText'); // create "newMessageEmo" and have it set to the ID messageText ...
+            newMessageEmo.innerHTML = `(yes.)`; // and write in that message area "(yes.)" ...
+            confetti.start(1500); // and add confetti!
+        } else { // if the genre does not include "EMO" ...
+            // console.log('no.'); // turn this on for testing
+            let newMessageEmo = document.getElementById('messageText'); // I still want to create the newMessageEmo ...
+            newMessageEmo.innerHTML = `(no.)`; // but this time, I want to get the message to return "(no.)"
         }
     })
-    .catch((err) => {
-        console.log(err);
+    .catch((err) => { // finally, we catch any errors ...
+        console.log(err); // and upload them to our console.
     });
 }); 
-// I need to go back to this and combine using Promises!!!
-
-
-
-/*---------------- Functions -------------------------------*/
-
 
 
 
